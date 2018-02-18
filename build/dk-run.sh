96ea860c243d5b33b9c30d1306f9518610f00f95
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-02-18 18:37:26 -0300

    Update dk-run.sh

--- build/dk-run.sh
+++ build/dk-run.sh
@@ -1,1 +1,3 @@
-sudo docker run -p 8080:8080 -v $PWD:/home -d --name nodedb node-db-server
+#https://hub.docker.com/_/couchdb/
+#https://rskupnik.github.io/docker_series_2_connecting_containers
+sudo docker run -p 8080:8080 -v $PWD:/home -d --network netdb --name nodedb node-db-server
