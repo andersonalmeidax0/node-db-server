18d20775955b2f20e22458f8a671a230abd074fe
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-02-18 23:27:56 -0300

    Update dk-testdb.sh

--- build/dk-testdb.sh
+++ build/dk-testdb.sh
@@ -1,1 +1,2 @@
+#    docker run -v /opt/couchdb/data:/opt/couchdb/data -p 5984:5984 -t -i --network netdb couchdb /bin/bash
 sudo docker run -p 5984:5984 -v /opt/couchdb/data:/opt/couchdb/data -t -i --net=netdb --name mycouchdb couchdb /bin/bash
