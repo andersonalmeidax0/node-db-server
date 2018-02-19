9847117ac6c4e61c74775943d2ff98fd1714a02b
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-02-18 23:22:15 -0300

    Update dk-test.sh

--- build/dk-test.sh
+++ build/dk-test.sh
@@ -1,1 +1,1 @@
-sudo docker run -p 8080:8080 -v $PWD:/home -t -i --network netdb --name nodedbtst node-base /bin/ash
+sudo docker run -p 8080:8080 -v $PWD:/home -t -i ---net=netdb  --name nodedbtst node-base /bin/ash
