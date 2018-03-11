b14f87be24a37c136e5f0b7659b57c548f7deb9c
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-03-11 15:52:20 -0300

    Update dk-creat2.sh

--- build/dk-creat2.sh
+++ build/dk-creat2.sh
@@ -2,12 +2,13 @@
 #curl https://raw.githubusercontent.com/andersonalmeidax0/node-db-server/master/build/dk-creat2.sh > dk-creat2.sh
 curl https://raw.githubusercontent.com/andersonalmeidax0/node-db-server/master/build/Dockerfile > Dockerfile
 curl https://raw.githubusercontent.com/andersonalmeidax0/node-db-server/master/build/Dockerfile.base > Dockerfile.base
-curl https://raw.githubusercontent.com/andersonalmeidax0/node-db-server/master/build/server.js > server.js
-curl https://raw.githubusercontent.com/andersonalmeidax0/node-db-server/master/build/index.html > index.html
+#curl https://raw.githubusercontent.com/andersonalmeidax0/node-db-server/master/build/server.js > server.js
+#curl https://raw.githubusercontent.com/andersonalmeidax0/node-db-server/master/build/index.html > index.html
 #o "." do comando abaixo eh o contexto
 docker build --no-cache -t="node-base" -f ./Dockerfile.base  .
 docker build --no-cache -t="node-db-server" .
 sudo docker push node-db-server
+sudo docker push node-base
 #docker run -d node-db-server
 
 
