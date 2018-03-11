cdbcf277e55e8f3ee0b1c095b0702ba04a2082c8
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-03-11 15:54:26 -0300

    Create dk-buildsrc.sh

---
+++ build/dk-buildsrc.sh
@@ -0,0 +1,4 @@
+#v2
+#deixa os files aqui. TODO: rodar testes unitarios
+curl https://raw.githubusercontent.com/andersonalmeidax0/node-db-server/master/build/server.js > server.js
+curl https://raw.githubusercontent.com/andersonalmeidax0/node-db-server/master/build/index.html > index.html
